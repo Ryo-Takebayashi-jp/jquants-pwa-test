b7af8aac7ac59d4f17276a8681649a9bd78efa69
Ryo-Takebayashi-jp <ryo.takebayashi@gmail.com>
2026-09-05 13:13:26 +0900

    ok

--- docs/CURRENT.md
+++
@@ -1,3 +0,0 @@
-v7e-alpha84
-
-Factor FY baseline resolver and Parity graduation candidate. See `history/README_alpha84.md`.
--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE="jq-pwa-v7e-alpha84";
-const ASSETS=["./","./index.html","./app.js?v=v7e-alpha84","./sqlite-worker.js?v=v7e-alpha84","./manifest.webmanifest","./release_history.json","./CHANGELOG.md"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
