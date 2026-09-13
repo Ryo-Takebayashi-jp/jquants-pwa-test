7363c299b2ae77ef5c7b85520ff7cc4a116a0b67
Ryo-Takebayashi-jp <ryo.takebayashi@gmail.com>
2026-09-13 22:31:02 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="jq-invest-v9.4";
+const CACHE="jq-invest-v9.6";
 const ASSETS=["./","./index.html","./app.js?v=v9.4","./sqlite-worker.js?v=v9.4","./manifest.webmanifest","./release_history.json","./CHANGELOG.md"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
